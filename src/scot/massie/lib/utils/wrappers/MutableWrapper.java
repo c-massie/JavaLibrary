9966aea13aba95f66c442ab800f69e00b64ebe76
Chris Massie <chris@massie.scot>
2021-03-11 22:39:59 +0000

    Added Javadoc to Wrapper and MutableWrapper
    Added Javadoc to Wrapper and MutableWrapper.

--- src/scot/massie/lib/utils/wrappers/MutableWrapper.java
+++ src/scot/massie/lib/utils/wrappers/MutableWrapper.java
@@ -1,7 +1,15 @@
 package scot.massie.lib.utils.wrappers;
 
+/**
+ * Wraps an item in an object. The item can be changed.
+ * @param <T> The type of the item in the wrapper.
+ */
 public class MutableWrapper<T> extends Wrapper<T>
 {
+    /**
+     * Wraps the given item in a mutable wrapper instance.
+     * @param item The item to be wrapped.
+     */
     public MutableWrapper(T item)
     { super(item); }
 
--- src/scot/massie/lib/utils/wrappers/Wrapper.java
+++ src/scot/massie/lib/utils/wrappers/Wrapper.java
@@ -1,10 +1,21 @@
 package scot.massie.lib.utils.wrappers;
 
+/**
+ * Wraps an item in an object.
+ * @param <T> The type of the object stored in the wrapper.
+ */
 public class Wrapper<T>
 {
+    /**
+     * Wraps the given item in a wrapper instance.
+     * @param item The item to be wrapped.
+     */
     public Wrapper(T item)
     { this.item = item; }
 
+    /**
+     * The item in the wrapper.
+     */
     protected T item;
 
     /**
