72e5b1bb3c07601627433da904309523e9f5e8b0
Chris Massie <chris@massie.scot>
2021-04-10 14:39:56 +0100

    Added @SafeVarargs annotations to MapUtils methods appropriately.
    Added @SafeVarargs annotations to MapUtils methods appropriately.

--- src/scot/massie/lib/collections/maps/MapUtils.java
+++ src/scot/massie/lib/collections/maps/MapUtils.java
@@ -18,6 +18,7 @@ public final class MapUtils
      * @param <V> The value of the keys of the maps.
      * @return A new map object where the item assigned to each key is the result of performing the provided operation
      */
+    @SafeVarargs
     public static <K, V> Map<K, V> accumulateValues(BinaryOperator<V> op, Map<K, V>... maps)
     {
         Map<K, V> result = new HashMap<>();
@@ -57,6 +58,7 @@ public final class MapUtils
      * @param <V> The value type of the maps.
      * @return A new map where all values are the comparative minimum across all passed maps for that key.
      */
+    @SafeVarargs
     public static <K, V extends Comparable<? super V>> Map<K, V> minValues(Map<K, V>... maps)
     { return accumulateValues((a, b) -> a.compareTo(b) <= 0 ? a : b, maps); }
 
@@ -79,6 +81,7 @@ public final class MapUtils
      * @param <V> The value type of the maps.
      * @return A new map where all values are the comparative maximum across all passed maps for that key.
      */
+    @SafeVarargs
     public static <K, V extends Comparable<? super V>> Map<K, V> maxValues(Map<K, V>... maps)
     { return accumulateValues((a, b) -> a.compareTo(b) <= 0 ? a : b, maps); }
 
@@ -101,6 +104,7 @@ public final class MapUtils
      * @return A new map object where the value assigned to each key is the sum of the values associated with the same
      *         key in all of the passed maps where there is a value associated with that key.
      */
+    @SafeVarargs
     public static <K> Map<K, Short> sumMatchingShortValues(Map<K, Short>... maps)
     { return accumulateValues((a, b) -> (short)(a + b), maps); }
 
@@ -123,6 +127,7 @@ public final class MapUtils
      * @return A new map object where the value assigned to each key is the sum of the values associated with the same
      *         key in all of the passed maps where there is a value associated with that key.
      */
+    @SafeVarargs
     public static <K> Map<K, Integer> sumMatchingIntValues(Map<K, Integer>... maps)
     { return accumulateValues(Integer::sum, maps); }
 
@@ -145,6 +150,7 @@ public final class MapUtils
      * @return A new map object where the value assigned to each key is the sum of the values associated with the same
      *         key in all of the passed maps where there is a value associated with that key.
      */
+    @SafeVarargs
     public static <K> Map<K, Long> sumMatchingLongValues(Map<K, Long>... maps)
     { return accumulateValues(Long::sum, maps); }
 
@@ -167,6 +173,7 @@ public final class MapUtils
      * @return A new map object where the value assigned to each key is the sum of the values associated with the same
      *         key in all of the passed maps where there is a value associated with that key.
      */
+    @SafeVarargs
     public static <K> Map<K, Float> sumMatchingFloatValues(Map<K, Float>... maps)
     { return accumulateValues(Float::sum, maps); }
 
@@ -189,6 +196,7 @@ public final class MapUtils
      * @return A new map object where the value assigned to each key is the sum of the values associated with the same
      *         key in all of the passed maps where there is a value associated with that key.
      */
+    @SafeVarargs
     public static <K> Map<K, Double> sumMatchingDoubleValues(Map<K, Double>... maps)
     { return accumulateValues(Double::sum, maps); }
 
@@ -211,6 +219,7 @@ public final class MapUtils
      * @return A new map object where the value assigned to each key is the result of multiplying together all of the
      * values associated with the same key in all of the passed maps where there is a value associated with that key.
      */
+    @SafeVarargs
     public static <K> Map<K, Short> multiplyMatchingShortValues(Map<K, Short>... maps)
     { return accumulateValues((a, b) -> (short)(a + b), maps); }
 
@@ -233,6 +242,7 @@ public final class MapUtils
      * @return A new map object where the value assigned to each key is the result of multiplying together all of the
      * values associated with the same key in all of the passed maps where there is a value associated with that key.
      */
+    @SafeVarargs
     public static <K> Map<K, Integer> multiplyMatchingIntValues(Map<K, Integer>... maps)
     { return accumulateValues((a, b) -> a * b, maps); }
 
@@ -255,6 +265,7 @@ public final class MapUtils
      * @return A new map object where the value assigned to each key is the result of multiplying together all of the
      * values associated with the same key in all of the passed maps where there is a value associated with that key.
      */
+    @SafeVarargs
     public static <K> Map<K, Long> multiplyMatchingLongValues(Map<K, Long>... maps)
     { return accumulateValues((a, b) -> a * b, maps); }
 
@@ -277,6 +288,7 @@ public final class MapUtils
      * @return A new map object where the value assigned to each key is the result of multiplying together all of the
      * values associated with the same key in all of the passed maps where there is a value associated with that key.
      */
+    @SafeVarargs
     public static <K> Map<K, Float> multiplyMatchingFloatValues(Map<K, Float>... maps)
     { return accumulateValues((a, b) -> a * b, maps); }
 
@@ -299,6 +311,7 @@ public final class MapUtils
      * @return A new map object where the value assigned to each key is the result of multiplying together all of the
      * values associated with the same key in all of the passed maps where there is a value associated with that key.
      */
+    @SafeVarargs
     public static <K> Map<K, Double> multiplyMatchingDoubleValues(Map<K, Double>... maps)
     { return accumulateValues((a, b) -> a * b, maps); }
 
