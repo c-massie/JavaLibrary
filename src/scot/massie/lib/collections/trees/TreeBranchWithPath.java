2332c89edf0043d6d3b20e25d163961481d2bee1
Chris Massie <chris@massie.scot>
2021-08-09 06:57:37 +0100

    Documented TreeBranchWithPath
    Added Javadoc to TreeBranchWithPath.

--- src/scot/massie/lib/collections/trees/TreeBranchWithPath.java
+++ src/scot/massie/lib/collections/trees/TreeBranchWithPath.java
@@ -2,20 +2,47 @@ package scot.massie.lib.collections.trees;
 
 import java.util.Objects;
 
+/**
+ * A pairing of a branch of a tree (as another tree) with the path that that branch's root path has in the tree it came
+ * from.
+ * @param <T> The type of the branch.
+ * @param <TNode> The type of the individual nodes in the tree paths.
+ * @param <TLeaf> The type of the items stored in the tree.
+ */
 public class TreeBranchWithPath<T extends Tree<TNode, TLeaf>, TNode, TLeaf>
 {
+    /**
+     * The tree branch.
+     */
     protected final T branch;
+
+    /**
+     * The path the branch's root path has in the tree it came from.
+     */
     protected final TreePath<TNode> path;
 
+    /**
+     * Creates a new TreeBranchWithPath.
+     * @param branch The branch.
+     * @param path The path of the branch's root path in the source tree.
+     */
     public TreeBranchWithPath(T branch, TreePath<TNode> path)
     {
         this.branch = branch;
         this.path = path;
     }
 
+    /**
+     * Gets the branch.
+     * @return The branch.
+     */
     public T getBranch()
     { return branch; }
 
+    /**
+     * Gets the path. This path represents the branch's root path's path in the source tree.
+     * @return The branch's path.
+     */
     public TreePath<TNode> getPath()
     { return path; }
 
