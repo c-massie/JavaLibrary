eb4dc630e829a2ceeb5516c5b39bb2c50a247670
c-massie <77603178+c-massie@users.noreply.github.com>
2021-09-22 21:15:29 +0100

    Documented ControlFlowUtils.getFromIf(...) and .getFromUnlessNull(...)
    Documented ControlFlowUtils.getFromIf(...) and .getFromUnlessNull(...)

--- src/scot/massie/lib/utils/ControlFlowUtils.java
+++ src/scot/massie/lib/utils/ControlFlowUtils.java
@@ -36,42 +36,117 @@ public final class ControlFlowUtils
         return null;
     }
 
+    /**
+     * Derives a value from a given value where a predicate is satisfied.
+     * @param from The value to derive a value from.
+     * @param ifStatement The predicate to satisfy.
+     * @param trueResultGetter The function to derive a value from the given value if the predicate is satisfied.
+     * @param falseResultGetter The function to derive a value from the given value if the predicate is not satisfied.
+     * @param <T> The type of the value a result is being derived from.
+     * @param <R> The type of the resulting value.
+     * @return The result of the appropriate getter.
+     */
     public static <T, R> R getFromIf(T from,
                                      Predicate<T> ifStatement,
                                      Function<T, R> trueResultGetter,
                                      Function<T, R> falseResultGetter)
     { return ifStatement.test(from) ? trueResultGetter.apply(from) : falseResultGetter.apply(from); }
 
+    /**
+     * Derives a value from a given value where a predicate is satisfied.
+     * @param from The value to derive a value from.
+     * @param ifStatement The predicate to satisfy.
+     * @param trueResultGetter The function to derive a value from the given value if the predicate is satisfied.
+     * @param falseResultGetter The function to produce a value if the predicate is not satisfied.
+     * @param <T> The type of the value a result is being derived from.
+     * @param <R> The type of the resulting value.
+     * @return The result of the appropriate getter.
+     */
     public static <T, R> R getFromIf(T from,
                                      Predicate<T> ifStatement,
                                      Function<T, R> trueResultGetter,
                                      Supplier<R> falseResultGetter)
     { return ifStatement.test(from) ? trueResultGetter.apply(from) : falseResultGetter.get(); }
 
+    /**
+     * Derives a value from a given value where a predicate is satisfied.
+     * @param from The value to derive a value from.
+     * @param ifStatement The predicate to satisfy.
+     * @param trueResultGetter The function to produce a value if the predicate is satisfied.
+     * @param falseResultGetter The function to derive a value from the given value if the predicate is not satisfied.
+     * @param <T> The type of the value a result is being derived from.
+     * @param <R> The type of the resulting value.
+     * @return The result of the appropriate getter.
+     */
     public static <T, R> R getFromIf(T from,
                                      Predicate<T> ifStatement,
                                      Supplier<R> trueResultGetter,
                                      Function<T, R> falseResultGetter)
     { return ifStatement.test(from) ? trueResultGetter.get() : falseResultGetter.apply(from); }
 
+    /**
+     * Derives a value from a given value where a predicate is satisfied.
+     * @param from The value to derive a value from.
+     * @param ifStatement The predicate to satisfy.
+     * @param trueResultGetter The function to produce a value if the predicate is satisfied.
+     * @param falseResultGetter The function to produce a value if the predicate is not satisfied.
+     * @param <T> The type of the value a result is being derived from.
+     * @param <R> The type of the resulting value.
+     * @return The result of the appropriate getter.
+     */
     public static <T, R> R getFromIf(T from,
                                      Predicate<T> ifStatement,
                                      Supplier<R> trueResultGetter,
                                      Supplier<R> falseResultGetter)
     { return ifStatement.test(from) ? trueResultGetter.get() : falseResultGetter.get(); }
 
+    /**
+     * Derives a value from a given unless where it's not null.
+     * @param from The value to derive a value from if it's not null.
+     * @param notNullResultGetter The function to derive a value from the given value if it's not null.
+     * @param nullResultGetter The function to produce a value if the given value is null.
+     * @param <T> The type of the value a result is being derived from.
+     * @param <R> The type of the resulting value.
+     * @return The result of the appropriate getter.
+     */
     public static <T, R> R getFromUnlessNull(T from, Function<T, R> notNullResultGetter, Supplier<R> nullResultGetter)
     { return from != null ? notNullResultGetter.apply(from) : nullResultGetter.get(); }
 
+    /**
+     * Derives a value from a given unless where it's not null.
+     * @param from The value to derive a value from if it's not null.
+     * @param notNullResultGetter The function to derive a value from the given value if it's not null.
+     * @param <T> The type of the value a result is being derived from.
+     * @param <R> The type of the resulting value.
+     * @return The result of the given getter if the given value is not null, or null if it is.
+     */
     public static <T, R> R getFromUnlessNull(T from, Function<T, R> notNullResultGetter)
     { return from != null ? notNullResultGetter.apply(from) : null; }
 
+    /**
+     * Derives a value from a given unless where it's not null.
+     * @param from The value to derive a value from if it's not null.
+     * @param notNullResultGetter The function to produce a value if the given value is not null.
+     * @param nullResultGetter The function to produce a value if the given value is null.
+     * @param <T> The type of the value being checked.
+     * @param <R> The type of the resulting value.
+     * @return The result of the appropriate getter.
+     */
     public static <T, R> R getFromUnlessNull(T from, Supplier<R> notNullResultGetter, Supplier<R> nullResultGetter)
     { return from != null ? notNullResultGetter.get() : nullResultGetter.get(); }
 
+    /**
+     * Derives a value from a given unless where it's not null.
+     * @param from The value to derive a value from if it's not null.
+     * @param notNullResultGetter The function to produce a value if the given value is not null.
+     * @param <T> The type of the value being checked.
+     * @param <R> The type of the resulting value.
+     * @return The result of the given getter if the given value is not null, or null if it is.
+     */
     public static <T, R> R getFromUnlessNull(T from, Supplier<R> notNullResultGetter)
     { return from != null ? notNullResultGetter.get() : null; }
 
+    
     public static <T> void ifVal(T val, Predicate<T> ifStatement, Consumer<T> trueAction, Consumer<T> falseAction)
     {
         if(ifStatement.test(val))
