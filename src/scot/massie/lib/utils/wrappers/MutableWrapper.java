de12962673dae68f62b30a61f0fed01f0f9891d7
Chris Massie <chris@massie.scot>
2021-12-06 23:44:53 +0000

    In MutableWrapper, added .setThenGet(T) and .getThenSet(T)
    In MutableWrapper, added, implemented, and documented .setThenGet(T) and .getThenSet(T)

--- src/scot/massie/lib/utils/wrappers/MutableWrapper.java
+++ src/scot/massie/lib/utils/wrappers/MutableWrapper.java
@@ -19,4 +19,24 @@ public class MutableWrapper<T> extends Wrapper<T>
      */
     public void set(T item)
     { this.item = item; }
+
+    /**
+     * Replaces the item in this wrapper with the provided item, and returns the new item.
+     * @param item The item to be put into this wrapper.
+     * @return The item in this wrapper as a result of this method call.
+     */
+    public T setThenGet(T item)
+    { return this.item = item; }
+
+    /**
+     * Replaces the item in this wrapper with the provided item, and returns the previous item.
+     * @param item The item to be put into this wrapper.
+     * @return The item previously in the wrapper before calling this method.
+     */
+    public T getThenSet(T item)
+    {
+        T old = this.item;
+        this.item = item;
+        return old;
+    }
 }
