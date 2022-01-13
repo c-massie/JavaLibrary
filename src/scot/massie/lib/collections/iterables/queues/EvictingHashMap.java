4200f9c228c4b494a24afd574d2f6d7bd18eaa6e
Chris Massie <chris@massie.scot>
2022-01-13 22:15:37 +0000

    Created EvictingHashMap
    Created EvictingHashMap

---
+++ src/scot/massie/lib/collections/iterables/queues/EvictingHashMap.java
@@ -0,0 +1,36 @@
+package scot.massie.lib.collections.iterables.queues;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Variant of {@link LinkedHashMap} that removes the oldest entries to make way for new entries, if a maximum size is
+ * specified.
+ * @param <K> The type of the key objects of the map.
+ * @param <V> The type of the value objects of the map.
+ */
+public class EvictingHashMap<K, V> extends LinkedHashMap<K, V>
+{
+    /**
+     * The maximum number of entries in the map.
+     */
+    private final int maxSize;
+
+    /**
+     * Creates a new evicting hashmap.
+     */
+    public EvictingHashMap()
+    { maxSize = Integer.MAX_VALUE - 1; }
+
+    /**
+     * Creates a new evicting hashmap with a specified maximum size. If an operation would result in this map having
+     * more entries than this specified size, the oldest entries will be removed to make way.
+     * @param maxSize The maximum number of entries allowed in this map.
+     */
+    public EvictingHashMap(int maxSize)
+    { this.maxSize = maxSize; }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest)
+    { return size() > maxSize; }
+}
